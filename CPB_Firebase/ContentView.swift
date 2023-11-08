1570a6e435cfe0a7006890e042cbb9372561447a
Teodor Djuric <tdjuric5081@stu.d214.org>
2023-11-08 10:47:12 -0600

    Initial Commit

---
+++ CPB_Firebase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CPB_Firebase
+//
+//  Created by Teodor Djuric on 11/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
